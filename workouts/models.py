74bcadd347757646abfe36eeedf2cd5b52fd2278
nisi_pop_desktop <49096264+niallsiow@users.noreply.github.com>
2025-04-11 09:30:26 +0100

    Move weight field to workout instead of set

--- workouts/models.py
+++ workouts/models.py
@@ -4,10 +4,10 @@ from django.db import models
 class Session(models.Model):
     user = models.ForeignKey("auth.User", on_delete=models.CASCADE)
     date = models.DateField(auto_now_add=True)
-    notes = models.TextField()
+    notes = models.TextField(blank=True)
 
     def __str__(self):
-        return self.date
+        return f"{self.date}"
 
 
 class Exercise(models.Model):
@@ -20,15 +20,15 @@ class Exercise(models.Model):
 class Workout(models.Model):
     session = models.ForeignKey(Session, on_delete=models.CASCADE)
     exercise = models.ForeignKey(Exercise, on_delete=models.CASCADE)
+    weight = models.DecimalField(max_digits=6, decimal_places=2)
 
     def __str__(self):
-        return f"{self.session}: {self.exercise}"
+        return f"{self.session}: {self.exercise} {self.weight}"
 
 
 class Set(models.Model):
     workout = models.ForeignKey(Workout, on_delete=models.CASCADE)
-    weight = models.DecimalField(max_digits=6, decimal_places=2)
     reps = models.IntegerField()
 
     def __str__(self):
-        return f"{self.weight} {self.reps} {self.workout}"
+        return f"{self.reps} {self.workout}"
