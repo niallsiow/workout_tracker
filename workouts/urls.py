7d55c3ef63c3137ed83d5ecdb213d64808cc3544
nisi_pop_desktop <49096264+niallsiow@users.noreply.github.com>
2025-04-11 10:06:11 +0100

    Add session detail view

--- workouts/urls.py
+++ workouts/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import SessionListView
+from .views import SessionListView, SessionDetailView
 
 urlpatterns = [
     path("", SessionListView.as_view(), name="home"),
+    path("<int:pk>/", SessionDetailView.as_view(), name="session_detail"),
 ]
--- workouts/views.py
+++ workouts/views.py
@@ -1,7 +1,12 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 from .models import Session
 
 class SessionListView(ListView):
     model = Session
     template_name = "home.html"
+
+
+class SessionDetailView(DetailView):
+    model = Session
+    template_name = "session_detail.html"
