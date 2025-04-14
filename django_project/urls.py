e32e796e217bbb69e7a8657205297562877c3018
nisi_pop_desktop <49096264+niallsiow@users.noreply.github.com>
2025-04-14 06:25:53 +0100

    Add login, logout and signup views

--- django_project/urls.py
+++ django_project/urls.py
@@ -19,6 +19,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path("accounts/", include("django.contrib.auth.urls")),
+    path("accounts/", include("accounts.urls")),
     path("admin/", admin.site.urls),
     path("", include("workouts.urls")),
 ]
